74f5c0c011837de9a0db4b54f5071b82586739de
Jessica Ernst <99251460+Belladonnixi@users.noreply.github.com>
2022-11-10 11:05:52 +0100

    Initial Commit

---
+++ CodingChallenge#2/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  Project: CodingChallenge#2
+//  ViewController.swift
+//
+//
+//  Created by Jessica Ernst on 10.11.22
+//
+/// Copyright © 2022 Jessica Ernst. All rights reserved.
+//
+
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
