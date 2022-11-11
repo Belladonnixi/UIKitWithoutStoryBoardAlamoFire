aeeff6e5d281a8c34a08f5c95f6e5208db33c64f
Jessica Ernst <99251460+Belladonnixi@users.noreply.github.com>
2022-11-11 20:12:40 +0100

    working tableView

--- CodingChallenge#2/ViewController.swift
+++ CodingChallenge#2/ViewController.swift
@@ -10,8 +10,14 @@
 
 
 import UIKit
+import Alamofire
 
 class ViewController: UIViewController {
+    
+    var dataList: [FakeApiData] = []
+    var cachedImages = [UIImage?]()
+    var selectedDataset: FakeApiData?
+    
     
     private let tableView: UITableView = {
         let tableView = UITableView()
@@ -22,9 +28,87 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        tableView.dataSource = self
+        tableView.delegate = self
+        
+        fetchFakeApiData()
+    }
+    
+    override func loadView() {
+        super.loadView()
+        
+        self.title = "CC Api Call"
+        view.backgroundColor = .systemBackground
+        view.addSubview(tableView)
+        
+        constraintsInit()
+    }
+    
+    
+    func constraintsInit() {
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: view.layoutMarginsGuide.topAnchor),
+            tableView.leadingAnchor.constraint(equalTo: view.layoutMarginsGuide.leadingAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.layoutMarginsGuide.bottomAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.layoutMarginsGuide.trailingAnchor)
+        ])
     }
 
 
 }
 
+extension ViewController: UITableViewDataSource, UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return dataList.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        let data = dataList[indexPath.row]
+        
+        var content = cell.defaultContentConfiguration()
+        content.text = data.title
+        content.secondaryText = data.url
+        
+        if self.cachedImages.indices.contains(indexPath.row) {
+            content.image = self.cachedImages[indexPath.row]
+        } else {
+            AF.request(data.thumbnailUrl).response{ response in
+                switch response.result {
+                case .success(let data):
+                    let image = UIImage(data: data!)
+                    self.cachedImages.append(image)
+                    tableView.reloadRows(at: [indexPath], with: .none)
+                    
+                case .failure(let error):
+                    print(error.localizedDescription)
+                }
+            }
+        }
+        
+        cell.contentConfiguration = content
+        return cell
+    }
+    
+    
+}
+
+extension ViewController {
+    
+
+    func fetchFakeApiData() {
+        
+        let url = "https://jsonplaceholder.typicode.com/photos"
+        
+        AF.request(url)
+            .validate()
+            .responseDecodable(of: [FakeApiData].self) { (response) in
+                guard let data = response.value else { return }
+                self.dataList = data
+                self.tableView.reloadData()
+
+            }
+    }
+}
---
+++ CodingChallenge#2/networking/FakeApiData.swift
@@ -0,0 +1,20 @@
+//
+//  Project: CodingChallenge#2
+//  Data.swift
+//
+//
+//  Created by Jessica Ernst on 11.11.22
+//
+/// Copyright © 2022 Jessica Ernst. All rights reserved.
+//
+
+
+import Foundation
+
+struct FakeApiData: Codable {
+    let albumId: Int
+    let id: Int
+    let title: String
+    let url: String
+    let thumbnailUrl: String
+}
